e3b6fe4189cff147c1fb4406e1bb3294f082aeec
Pratap Padekar <unitedpratap7@gmail.com>
2023-04-19 19:17:11 +0530

    Updated the App

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,23 @@ import RegisterPage from './pages/RegisterPage'
 
 const App = () => {
   return (
-    // <div>
-    //   <Routes>
-    //     <Route path='/' element={<Homepage />} />
-    //     {/* <Route path='/products/cat/:catId' element={<ProductsPage />} />
-    //     <Route path='/products/details/:_id' element={<ProductDetailsPage />}  />
-    //     <Route path='/products/' element={<AllProductsPage />}  /> */}
-    //     {/* Nested routes -> */}
-    //     <Route path='/products'>
-    //         <Route index element={<AllProductsPage />} />
-    //         <Route path='details/:_id' element={<ProductDetailsPage />} />
-    //         <Route path='cat/:catId' element={<ProductsPage />} />
-    //     </Route>
-    //     <Route path='/about' element={<AboutPage />}  />
-    //     <Route path='/contact' element={<ContactsPage />}  />
-    //   </Routes>
-    // </div>
-    <RegisterPage />
+    <div>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        {/* <Route path='/products/cat/:catId' element={<ProductsPage />} />
+        <Route path='/products/details/:_id' element={<ProductDetailsPage />}  />
+        <Route path='/products/' element={<AllProductsPage />}  /> */}
+        {/* Nested routes -> */}
+        <Route path='/products'>
+            <Route index element={<AllProductsPage />} />
+            <Route path='details/:_id' element={<ProductDetailsPage />} />
+            <Route path='cat/:catId' element={<ProductsPage />} />
+        </Route>
+        <Route path='/about' element={<AboutPage />}  />
+        <Route path='/contact' element={<ContactsPage />}  />
+        <Route path='/register' element={<RegisterPage />}  />
+      </Routes>
+    </div>
   )
 }
 
