171943d2d0656688e1ce21c30e321a450114799a
Pratap Padekar <unitedpratap7@gmail.com>
2023-04-23 16:33:53 +0530

    added the responsive behaviour

--- src/components/ItemInCart.jsx
+++ src/components/ItemInCart.jsx
@@ -12,17 +12,20 @@ const ItemInCart = ({cartItem}) => {
     }
 
   return (
-    <div className='my-5 flex gap-[3rem]'>
-        <div>
+    <div className='my-5 flex gap-[3rem] flex-col sm:flex-row'>
+        <div className='self-center sm:self-stretch'>
             <img className='w-[350px] h-[300px] rounded-lg' src={IMAGE_URL + cartItem.image} alt={cartItem.productName} />
         </div>
-        <div>
+        <div className='self-center sm:self-stretch'>
             <h3 className='text-xl'>{cartItem.productName}</h3>
             <div className='mt-5'>
             <button onClick={() => dispatch(DECREASE_QUANTITY(cartItem))} className='bg-gray-100 px-3 py-1 rounded-lg text-black font-bold'>-</button>
             <div className='inline mx-5'>{cartItem.quantity}</div>
             <button onClick={() => dispatch(INCREASE_QUANTITY(cartItem))} className='bg-gray-100 px-3 py-1 rounded-lg text-black font-bold'>+</button>
-            <div className='inline ml-5'>
+            <div className='lg:inline ml-5 hidden'>
+                <button onClick={() => dispatch(REMOVE_FROM_CART(cartItem))} className='px-3 py-3 bg-red-500 text-white rounded-lg'><HiOutlineTrash size={20} /></button>
+            </div>
+            <div className='block lg:hidden mt-5'>
                 <button onClick={() => dispatch(REMOVE_FROM_CART(cartItem))} className='px-3 py-3 bg-red-500 text-white rounded-lg'><HiOutlineTrash size={20} /></button>
             </div>
             </div>
@@ -30,7 +33,7 @@ const ItemInCart = ({cartItem}) => {
                 <p className='text-lg font-medium'>Product Price: &#8377; {cartItem.price}</p>
             </div>
         </div>
-        <div className='ml-auto'>
+        <div className='sm:ml-auto self-center sm:self-stretch'>
             <p className='text-2xl font-bold font-mono'>&#8377;{totalPrice()}</p>
         </div>
     </div>
